acbe42eaa412bf46cc789690c41df5d6399e2f80
addych <alexnicoljr@gmail.com>
2020-11-25 23:57:55 -0300

    atualização nas classes VO

--- src/br/edu/ufersa/sei/model/VO/DiretorVO.java
+++ src/br/edu/ufersa/sei/model/VO/DiretorVO.java
@@ -1,5 +1,18 @@
 package br.edu.ufersa.sei.model.VO;
 
 public class DiretorVO extends UsuarioVO{
-	
+	private long idDir;
+
+	//getters and setters
+	public long getIdDir() {
+		return idDir;
+	}
+
+	public void setIdDir(long idDir) {
+		if(idDir >= 0) {
+			this.idDir = idDir;
+		}else {
+			System.out.println("Id do Diretor Inválido!");
+		}
+	}
 }
