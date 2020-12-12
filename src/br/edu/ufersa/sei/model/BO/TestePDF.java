4aa9b0ae20e135e5acf7d003d8f01281aa95f3c4
Geyse <55860462+geyseevelyn@users.noreply.github.com>
2020-12-12 17:19:55 -0300

    teste pdf

--- src/br/edu/ufersa/sei/model/BO/TestePDF.java
+++ src/br/edu/ufersa/sei/model/BO/TestePDF.java
@@ -6,10 +6,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Stream;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -36,35 +40,30 @@ public class TestePDF {
 			 
 			 try {
 				  PdfWriter.getInstance(doc, new FileOutputStream(arquivoPdf));
+				  
 				  doc.open();
 				  
+				  doc.setPageSize(PageSize.A4);
+			
+//				  Image imagem = Image.getInstance("C:\\Users\\Notebook\\Documents\\UFERSA\\POO\\eclipse-workspace\\ProjetoPOO\\src\\br\\edu\\ufersa\\sei\\view\\VE\\logotelas.png");
+//				  imagem.scaleToFit(100, 100);
+//				  doc.add(imagem);
+//				
 				  Paragraph title = new Paragraph("Lista de Alunos");
 				  title.setAlignment(1);
 				  doc.add(title);
-				  Paragraph p = new Paragraph(" ");
-				  doc.add(p);
+				  doc.add(new Paragraph(" ")); //pular linha
 				  
 				  PdfPTable tabela = new PdfPTable(5);  //número de colunas da tabela
-			        
-				  PdfPCell cell = new PdfPCell(new Paragraph("Nome"));
-				  PdfPCell cell2 = new PdfPCell(new Paragraph("CPF"));
-				  PdfPCell cell3 = new PdfPCell(new Paragraph("Email"));
-				  PdfPCell cell4 = new PdfPCell(new Paragraph("Endereço"));
-				  PdfPCell cell5 = new PdfPCell(new Paragraph("Turma"));
-				
-				  tabela.addCell(cell);
-				  tabela.addCell(cell2);
-				  tabela.addCell(cell3);
-				  tabela.addCell(cell4);
-				  tabela.addCell(cell5);
+			      addTableHeader(tabela);
 				  
 				  for(AlunoVO alu : alunosList) {
 					  
-					  cell = new PdfPCell(new Paragraph(alu.getNome() + ""));
-					  cell2 = new PdfPCell(new Paragraph(alu.getCpf() + ""));
-					  cell3 = new PdfPCell(new Paragraph(alu.getEmail() + ""));
-					  cell4 = new PdfPCell(new Paragraph(alu.getEndereco() + ""));
-					  cell5 = new PdfPCell(new Paragraph(alu.getTurma().getIdTurma() + ""));
+					  PdfPCell cell = new PdfPCell(new Paragraph(alu.getNome() + ""));
+					  PdfPCell cell2 = new PdfPCell(new Paragraph(alu.getCpf() + ""));
+					  PdfPCell cell3 = new PdfPCell(new Paragraph(alu.getEmail() + ""));
+					  PdfPCell cell4 = new PdfPCell(new Paragraph(alu.getEndereco() + ""));
+					  PdfPCell cell5 = new PdfPCell(new Paragraph(alu.getTurma().getIdTurma() + ""));
 					
 					  tabela.addCell(cell);
 					  tabela.addCell(cell2);
@@ -76,21 +75,28 @@ public class TestePDF {
 				  doc.add(tabela);
 				  doc.close();
 				  
-				  try {
-					   Desktop.getDesktop().open(new File(arquivoPdf));
-				  } catch (IOException e) {
-					e.printStackTrace();
-				  }
+				  Desktop.getDesktop().open(new File(arquivoPdf));  //abrir o documento automaticamente
 				
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (DocumentException e) {
+				e.printStackTrace(); 
+			} catch (IOException e) {
 				e.printStackTrace();
-			}		 
-			
+			}
+		  
 		} catch (InsertException e) {
 			e.printStackTrace();
 		}
+	}	
+
+	private static void addTableHeader(PdfPTable table) {
+	    Stream.of("Nome", "CPF", "Email", "Endereço", "Turma")
+	      .forEach(columnTitle -> {
+	        PdfPCell header = new PdfPCell();
+	        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+	        header.setPhrase(new Phrase(columnTitle));
+	        table.addCell(header);
+	    });
 	}
-	
 }
